a17c507fe917fa8282e8ce5933d791e21412ff6e
hsrsbd <hsraffety@gmail.com>
2019-09-27 17:17:58 -0700

    05 lists exercise complete.

--- src/05_lists.py
+++ src/05_lists.py
@@ -8,18 +8,20 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE 
+print(x.append(4))
 print(x)
-
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE 
+print(x.extend(y))
 print(x)
-
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE 
+print(x.remove(8))
 print(x)
-
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE 
+print(x.insert(5, 99))
+
 print(x)
 
 # Print the length of list x
