863a98118f5893f548db5dac3bb01ea5e804b561
Hunter Raffety <hsraffety@gmail.com>
2019-09-30 20:17:48 -0700

    Lists, tuples, and slices

--- src/05_lists.py
+++ src/05_lists.py
@@ -14,24 +14,26 @@ print(x)
 # YOUR CODE HERE 
 print(x.extend(y))
 print(x)
+
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE 
 print(x.remove(8))
 print(x)
+
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE 
 print(x.insert(5, 99))
-
 print(x)
 
 # Print the length of list x
 # YOUR CODE HERE 
 print(len(x))
+
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
-
 def multiply(x):
   return x * 1000
+
 multiplied = map(multiply, x)
 numbers = set(multiplied)
 print(numbers)
--- src/06_tuples.py
+++ src/06_tuples.py
@@ -30,15 +30,14 @@ b = (-14, 72)
 # Prints "Distance is 66.94"
 print("Distance is: {:.2f}".format(dist(a, b)))
 
-
-
 # Write a function `print_tuple` that prints all the values in a tuple
-
+def print_tuple(tuple):
+  print(tuple)
 # YOUR CODE HERE
 
 t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
 
 # Declare a tuple of 1 element then print it
-u = (1)  # What needs to be added to make this work?
+u = (1,)  # What needs to be added to make this work?
 print_tuple(u)
